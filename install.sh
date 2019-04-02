3ebee7d17eabcba0d5e5d9a4dc9cc5e8ac64cec8
Tim Hall <tim.hall.engr@gmail.com>
2019-04-02 11:41:44 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-app_dir="$HOME/Library/Application Support/vba-blocks"
+lib_dir="$HOME/Library/Application Support/vba-blocks"
 bin_dir="$lib_dir/bin"
 addins_dir="$lib_dir/addins/build"
 addins_link="$HOME/vba-blocks Add-ins"
@@ -25,27 +25,35 @@ else
 fi
 
 if [ ! -d "$lib_dir" ]; then
+  echo "Creating lib directory $lib_dir"
 	mkdir -p "$lib_dir"
 fi
 
+echo "Downloading vba-blocks..."
+echo "($release_uri)"
 curl -fL# -o "$zip_file" "$release_uri"
-tar -xzf "$zip_file"
+
+echo ""
+echo "Extracting vba-blocks..."
+tar -xzf "$zip_file" --directory "$lib_dir"
 chmod +x "$bin_dir/vba-blocks"
 chmod +x "$bin_dir/vba"
+chmod +x "$lib_dir/vendor/node"
 
 # Add bin to .profile / .bash_profile
-if ! [ -a $profile ] || ! grep -q $bin_dir $profile; then
-  echo "Adding $export_bin to $profile"
+if ! [ -a $profile ] || ! grep -q "$bin_dir" $profile; then
+  echo "Adding vba-blocks to $profile"
   echo $export_bin >> "$profile"
 fi
-if [ -a $bash_profile ] && ! grep -q $bin_dir $bash_profile; then
-  echo "Adding $export_bin to $bash_profile"
+if [ -a $bash_profile ] && ! grep -q "$bin_dir" $bash_profile; then
+  echo "Adding vba-blocks to $bash_profile"
   echo $export_bin >> "$bash_profile"
 fi
 
 # Create accessible add-ins folder
-echo "Linking $addins_dir to $addins_link"
+echo "Creating link to add-ins at \"$addins_link\"..."
 ln -sf "$addins_dir" "$addins_link"
 
-echo "vba-blocks was installed successfully to $lib_dir"
+echo ""
+echo "vba-blocks was installed successfully!"
 echo "Run 'vba --help' to get started"
